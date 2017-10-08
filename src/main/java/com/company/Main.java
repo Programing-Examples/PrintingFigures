e8f13d675c5896807b8739c630b19bfacf4f5be2
01omar10 <omardiaz88@gmail.com>
2017-10-08 11:17:49 +0200

    Print a triangle

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -8,13 +8,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int size;
 
-        System.out.println("Tamaño del cuadradro: ");
+        System.out.println("Tamaño de la base: ");
         size = scanner.nextInt();
 
+        //Square
+        // \n print an end line
+        System.out.println("Cuadrado: \n");
         for (int i=0; i<size; i++) {
             // \t is a tab (4 blank spaces) just to center our square
             System.out.print("\t\t\t");
             for (int j=0; j<size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        //Triangle
+        System.out.println("Triangulo: \n");
+        for (int i=size; i>0; i--) {
+            System.out.print("\t\t\t");
+            for (int j=i; j <= size; j++) {
                 System.out.print("* ");
             }
             System.out.println();
